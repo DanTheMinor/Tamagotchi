133d9bdf535a439fe26196e69d04032eef9ac14b
Ben & Dan <student@epicodus.com>
2015-04-28 12:03:55 -0700

    Created and passed spec for determining the current age of a tamagotchi.

--- lib/tamagotchi.rb
+++ lib/tamagotchi.rb
@@ -25,6 +25,15 @@ class Tamagotchi
   end
 
   define_method(:birth_date) do
-    "#{@birth_date.hour}:#{@birth_date.min}"
+    "#{@birth_date.min}:#{@birth_date.sec}"
+  end
+
+  define_method(:age) do
+    current_time = Time.new()
+    age_min = current_time.min - @birth_date.min
+    age_sec = current_time.sec - @birth_date.sec
+    minutes = age_min < 10 ? "0#{age_min}" : "#{age_min}"
+    seconds = age_sec < 10 ? "0#{age_sec}" : "#{age_sec}"
+    statement = "#{minutes}:#{seconds}"
   end
 end
--- spec/tamagotchi_spec.rb
+++ spec/tamagotchi_spec.rb
@@ -14,9 +14,16 @@ describe(Tamagotchi) do
     it("sets the birth date to the current time") do
       test_pet = Tamagotchi.new("big dragon")
       current_time = Time.new()
-      expect(test_pet.birth_date()).to(eq("#{current_time.hour()}:#{current_time.min()}"))
+      expect(test_pet.birth_date()).to(eq("#{current_time.min()}:#{current_time.sec()}"))
     end
 
   end
 
+  describe("#age") do
+    it("calculates the age of the tamagotchi") do
+      test_pet = Tamagotchi.new("big dragon")
+      sleep(3)
+      expect(test_pet.age()).to(eq("00:03"))
+    end
+  end
 end
